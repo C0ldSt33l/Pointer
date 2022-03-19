56503d0ce5a3d615097fe01896fe04b75b36dc20
C0ldSt33l <sychev2003di@gmail.com>
2022-03-19 10:46:06 +0300

    Добавьте файлы проекта.

---
+++ LAB№18/Source.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+//”пор€дочить три числа по возрастанию их модулей.
+
+void swap(int *a, int *b)
+{
+	if (abs(*a) > abs(*b))
+	{
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+}
+
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+
+	int a, b, c;
+	
+	
+	printf("¬ведите 3 числа:\n");
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	scanf_s("%d", &c);
+	
+	swap(&a, &b);
+	swap(&b, &c);
+	
+	printf("\n%d\n%d\n%d\n", a, b, c);
+
+	return 0;
+}
